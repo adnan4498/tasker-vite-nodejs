da4f0523f2fd9e6362c0ff94e4be4644f33fdc08
adnan4498 <akhan123456008@gmail.com>
2025-06-12 19:27:57 +0500

    parsedJSONBody promise function created

--- todo-nodjs-server/server/index.js
+++ todo-nodjs-server/server/index.js
@@ -5,25 +5,26 @@ let server = http.createServer((req, res) => {
   let method = req.method;
   let url = req.url;
 
-  console.log(method, "method");
-  console.log(url, "url");
-
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   let parsedJSONBody = function (req) {
     return new Promise((resolve, reject) => {
-      let body = ""
+      let body = "";
 
       req.on("data", (chunk) => {
-        body += String(chunk)
-      })
+        body += String(chunk);
+      });
 
       req.on("end", () => {
-        
-      })
-
+        try {
+          let parseBody = JSON.parse(body);
+          resolve(parseBody);
+        } catch (error) {
+          reject(error);
+        }
+      });
     });
   };
 
@@ -37,44 +38,54 @@ let server = http.createServer((req, res) => {
     res.end();
     return;
   } else if (method == "POST" && url == "/api/todos") {
-    // let body;
-    // req.on("data", (chunk) => {
-    //   body = String(chunk);
+
+    // parsedJSONBody(req).then((body) => {
+    //   req.on("end", () => {
+    //     console.log(body, "body")
+    //     let parsed;
+    //     try {
+    //       parsed = body;
+    //     } catch (error) {
+    //       res.statusCode = 400;
+    //       res.end(error, "invalid JSON");
+    //     }
+
+    //     console.log(parsed, "ppp")
+    //     res.writeHead(200, {"content-type" : "application/json"})
+    //     res.end({parsed})
+    //   });
     // });
 
-    let parsed = parsedJSONBody(req);
-    console.log(parsed, "pppp ");
+    parsedJSONBody(req).then((body) => {
+      console.log(body, "bbb")
+    })
 
-    req.on("end", () => {
-      // let parsed;
-      // try {
-      //   parsed = JSON.parse(body);
-      // } catch (err) {
-      //   res.writeHead(400, { "Content-Type": "application/json" });
-      //   return res.end(JSON.stringify({ error: "Invalid JSON" }));
-      // }
-
-      let { newTodo, existingTodos } = parsed;
-
-      if (newTodo.length == 0) {
-        res.writeHead(400, { "content-type": "text/html" });
-        res.end();
-      } else {
-        res.writeHead(200, { "content-type": "application/json" });
-        let getLastId =
-          existingTodos.length != 0 ? existingTodos.slice(-1)[0].id : 0;
-
-        let mergedTodos = [
-          ...existingTodos,
-          { id: ++getLastId, todoVal: newTodo },
-        ];
-        res.end(JSON.stringify(mergedTodos));
-      }
-    });
+    // req.on("end", () => {
+    // let parsed;
+    // try {
+    //   parsed = JSON.parse(body);
+    // } catch (err) {
+    //   res.writeHead(400, { "Content-Type": "application/json" });
+    //   return res.end(JSON.stringify({ error: "Invalid JSON" }));
+    // }
+    // let { newTodo, existingTodos } = parsed;
+    // if (newTodo.length == 0) {
+    //   res.writeHead(400, { "content-type": "text/html" });
+    //   res.end();
+    // } else {
+    //   res.writeHead(200, { "content-type": "application/json" });
+    //   let getLastId =
+    //     existingTodos.length != 0 ? existingTodos.slice(-1)[0].id : 0;
+    //   let mergedTodos = [
+    //     ...existingTodos,
+    //     { id: ++getLastId, todoVal: newTodo },
+    //   ];
+    //   res.end(JSON.stringify(mergedTodos));
+    // }
+    // });
   } else if (method == "PUT" && url.includes("update")) {
     let urlParts = url.split("/");
     let todoId = urlParts.pop();
-    console.log(todoId, "tttt");
 
     res.writeHead(200, { "content-type": "application/json" });
   } else {
